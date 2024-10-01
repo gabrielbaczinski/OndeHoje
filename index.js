e34114357622d2b218d9d4a699962c6c6b1cf573
gabrielbaczinski <gabrielbaczinski@gmail.com>
2024-09-30 22:19:36 -0300

    SQL e Update de Iframe

--- index.js
+++ index.js
@@ -4,28 +4,64 @@ app.use(express.json());
 
 app.use(express.static('./pages'));
 
+
+var mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "10.150.11.80",
+    user: "root",
+    password: "1234", 
+    database: "webdevep"   
+});
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 const produtos = [];
-const usuarios = [];
+
+let idUsuarios = 0;
 
 const router = express.Router();
-router.get('/api/produtos', (req, res) => {  
-    console.log('entrou no get');
+router.get('/api/produtos', (req, res) => {
     res.status(200).json(produtos);
 });
-router.get('/api/usuarios', (req, res) => {  
-    res.status(200).json(usuarios);
-});
-router.post('/api/produtos', (req, res) => {  
+router.post('/api/produtos', (req, res) => {
     var produto = req.body;
     produto.id = 1;
     produtos.push(produto);
     res.status(201).json(produto);
 });
-router.post('/api/usuarios', (req, res) => {  
+
+router.get('/api/usuarios', (req, res) => {
+    let sql = "SELECT u.id, u.email, u.status FROM usuario u";
+    con.query(sql, function (err, result) {
+        if (err) throw err;        
+        res.status(200).json(result);
+    });       
+});
+router.post('/api/usuarios', (req, res) => {
     var usuario = req.body;
-    usuario.id = 1;
-    usuarios.push(usuario);
+    var sql = `INSERT INTO usuario (email, senha, status) VALUES 
+    ('${usuario.email}', '${usuario.senha}','${usuario.status ? 1 : 0}')`;
+    con.query(sql, function (err, result) {
+        if (err) throw err;        
+    });
     res.status(201).json(usuario);
+});
+router.get('/api/usuarios/:id', (req, res) => {
+    const id = req.param("id");
+    console.log(id);
+    const resultado = usuarios.filter(u => u.id == id);
+    res.status(200).json(resultado[0]);
+});
+router.delete('/api/usuarios/:id', (req, res) => {
+    const id = req.param("id");
+    console.log(id);
+
+    var removeIndex = usuarios.map(item => item.id).indexOf(id);
+    usuarios.splice(removeIndex, 1);
+
+    res.status(200).send(`usuario com id ${id} excluído`);
 });
 
 app.use(router);
--- pages/LugaresProximos/lugaresproximos.js
+++ pages/LugaresProximos/lugaresproximos.js
@@ -1,7 +1,7 @@
 var menuButton = document.getElementById("menu-button");
 var menu = document.getElementById("menu");
 
-menuButton.addEventListener("click", function() {
+menuButton.addEventListener("click", function () {
   if (menu.classList.contains("menu-show")) {
     menu.classList.remove("menu-show");
     menu.classList.add("menu-hide");
@@ -9,4 +9,29 @@ menuButton.addEventListener("click", function() {
     menu.classList.remove("menu-hide");
     menu.classList.add("menu-show");
   }
+});
+
+function updateMap(location) {
+  var mapIframe = document.getElementById('map-iframe');
+  mapIframe.src = 'https://maps.google.com/maps?q=' + location + '&t=&z=15&ie=UTF8&iwloc=&output=embed';
+}
+
+document.getElementById('yada-link').addEventListener('click', function (event) {
+  event.preventDefault();
+  updateMap('q=Yada+Yada+Yada+-+Lanches,+Bar+e+Entretenimento');
+});
+
+document.getElementById('pedrao-link').addEventListener('click', function (event) {
+  event.preventDefault();
+  updateMap('Pedr%C3%A3o+Bar');
+});
+
+document.getElementById('boteco-link').addEventListener('click', function (event) {
+  event.preventDefault();
+  updateMap('Boteco+do+Didi');
+});
+
+document.getElementById('taverna-link').addEventListener('click', function (event) {
+  event.preventDefault();
+  updateMap('Taverna+do+Dragao');
 });
