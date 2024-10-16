ffc99f9571afd6c3d128481881715a277064a3b1
gabrielbaczinski <gabrielbaczinski@gmail.com>
2024-10-16 11:42:09 -0300

    Tela de config e php

---
+++ ClinicaA-master/bd/conectaBD.php
@@ -0,0 +1,24 @@
+<!-------------------------------------------------------------------------------
+Oficina Desenvolvimento Web
+PUCPR
+
+CONECTABD.PHP - deve ser incluído em todos os arquivos PHP que precisam de acesso à BD
+
+Profa. Cristina V. P. B. Souza
+Agosto/2022
+---------------------------------------------------------------------------------->
+<?php
+global $servername ;
+global $root;
+global $password;
+global $database;
+
+$servername = "localhost:3306";
+$username = "root";
+$password = "1234";
+$database = "ClinicaA";
+?>
+
+
+
+
